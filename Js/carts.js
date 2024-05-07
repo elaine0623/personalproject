285b9451471ff4e847bf60affff8d81c4908834e
elaine0623 <0148004@gmail.com>
2024-05-07 18:05:27 +0800

    修改導覽列css樣式/新增goTop/購物車小計(初步)

--- Js/carts.js
+++ Js/carts.js
@@ -1,25 +1,17 @@
-// $('#toggle').click(function(e){
-//     var checkboxes = document.querySelectorAll('.checkbox');
-//     var selected = this.checked;
-//     for (var i = 0; i < checkboxes.length; i++) {
-//         if (checkboxes[i] != selected)
-//             checkboxes[i].checked = selected;
-//     }
-// })
 //設定全選及全部取消功能
 function toggle(toggle_status) {
-    var checkboxes = document.querySelectorAll('.item_check');
-    console.log(toggle_status)
+    var checkboxes = document.querySelectorAll('.item_check');//找到購物車內所有選取項目
+    // console.log(toggle_status)
     for (var i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i] != toggle_status){
-            checkboxes[i].checked = toggle_status.checked;
+            checkboxes[i].checked = toggle_status.checked;//將toggle的狀態指派給各個購物車子項
         }
     }
 }
 
 //取得本機資料庫加入購物車項目
-const shoplist = (localStorage.getItem('cartItemid')).split('|');
-const selectData = shoplist.slice();
+const shoplist = (localStorage.getItem('cartItemid')).split('|');//將字串轉換成array
+const selectData = shoplist.slice();//複製array給selectData陣列
 console.log(selectData)
 
 // const selectData = [1,2,3,4,5,6,7,8,9];
@@ -91,7 +83,8 @@ var travelData = [
 ];
 let sum = 0;
 for(let i = 0;i<selectData.length;i++){
-    let item = travelData.find(item => item.id == selectData[i]); //找到商品的ID指派給item
+    let item = travelData.find(item => item.id == selectData[i]); //找到商品ID的Object指派給item
+    //console.log(item)
     $('#allData').append(`<div class="inner">
                     <div class="basic_info">
                         <div class="checkbox">
@@ -119,29 +112,32 @@ for(let i = 0;i<selectData.length;i++){
                 </div>`)
                 //發生change事件呼叫selectNum函數
                 $(`#total_${item.id}`).text(item.price);//一進入頁面就預設1個的價格
-                sum += item.price;
-                if (i==selectData.length-1){
-                    $('#sumall').text(sum)
-                    $('#sumall_inner').text(sum)
+                sum += item.price;//迴圈加總
+                if (i==selectData.length-1){//迴圈跑到最後一次時
+                    $('#sumall').text(sum)//在畫面上顯示
+                    $('#sumall_inner').text(sum)//同步存一個價格
                 }
+                //使用jQuery datepicker套件
                 $( `#datepicker_${item.id}` ).datepicker({
                     dateFormat:"yy/mm/dd",
                     defaultDate: "+1w",
                     changeMonth: true,
                     onSelect: function() { 
                         var dateObject = $(this).datepicker('getDate'); 
-                        var formattedDate = $.datepicker.formatDate("yy/mm/dd", dateObject);
+                        //alert (dateObject); 用datepicer('getDate')抓到user選擇日期指派給dateObject
+                        var formattedDate = $.datepicker.formatDate("yy/mm/dd", dateObject);//將dateObject的格式與startData的顯示一致
                         let itemlist = travelData.find(itemlist => itemlist.startData == formattedDate);
-                        var num = $(`#selectpeople_${item.id}`).val()
-                        old = $(`#total_${item.id}`).text();
-                        newdata = itemlist.price * num;
-                        $(`#total_${item.id}`).text(newdata);            
-                        between = newdata - old;
-                        sumtotal = +($('#sumall').text());
-                        $('#sumall').text(sumtotal + between)
+                        //console.log(itemlist);//尋找相同出發日的Object指派給{itemlist}
+                        var num = $(`#selectpeople_${item.id}`).val()//取user選擇人數的值
+                        old = $(`#total_${item.id}`).text();//取目前價格
+                        newdata = itemlist.price * num;//新Object的值*人數
+                        $(`#total_${item.id}`).text(newdata);//印出更新的小計         
+                        between = newdata - old;//兩者價差加到總計
+                        sumtotal = +($('#sumall').text());//取得目前總計的金額
+                        $('#sumall').text(sumtotal + between)//印出更新後的總計
                         $('#sumall_inner').text(sumtotal +between)
                     },
-                    beforeShowDay: function(date) {
+                    beforeShowDay: function(date) {//使用套件提供function限制可選天數
                         switch (item.id) {
                         case 1:
                         case 4:
@@ -196,17 +192,18 @@ for(let i = 0;i<selectData.length;i++){
                         }
                     }
                 });
-
+                //預設畫面初始日期
                 $( `#datepicker_${item.id}` ).datepicker( "setDate", `${item.startData}` );
 };
-
+//onchange="selectNum(${item.id})"
 function selectNum(id){
     var num = $(`#selectpeople_${id}`).val()
     let price = travelData.find(itemlist => itemlist.startData == $(`#datepicker_${id}`).val()).price;
+    //alert (price);//要找到當天日期對應到的價格
     //錯的price = +($(`#total_${id}`).text())
-    $(`#total_${id}`).text(price * num);
+    $(`#total_${id}`).text(price * num);//小計等於數量*價格
     // $('#sumall').text()
-    let sumall = +($('#sumall_inner').text())
-    sumall += price * (num-1) ;
+    let sumall = +($('#sumall_inner').text())//取得原始價格
+    sumall += price * (num-1) ;//總計會等於數量- 1(起始值)
     $('#sumall').text(sumall);
 }
